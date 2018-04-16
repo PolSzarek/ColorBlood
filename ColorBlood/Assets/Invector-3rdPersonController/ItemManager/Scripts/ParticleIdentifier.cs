a47e6d830a8817e7cd10d5a5698c74317b89a5f5
pauill_c <git@git.epitech.eu>
2018-04-16 22:57:33 +0900

    add script fx system

---
+++ ColorBlood/Assets/Invector-3rdPersonController/ItemManager/Scripts/ParticleIdentifier.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Invector.vItemManager
+{
+	public class ParticleIdentifier : MonoBehaviour {
+
+		public WeaponParticleType landmark;
+	}
+}
